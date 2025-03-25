831e553997f3a5273c2ec70c3233ee0ad69513b7
Testando0 <joh769637@gmail.com>
2025-03-25 11:53:19 -0300

    Update koyeb.js

--- koyeb.js
+++ koyeb.js
@@ -1,9 +1,11 @@
-FROM fedora:37
-
-RUN sudo dnf -y update &&\
-    sudo dnf install -y https://mirrors.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://mirrors.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm &&\
-    sudo dnf install -y git ffmpeg ImageMagick nodejs yarnpkg libwebp &&\
-    sudo dnf clean all -y
+FROM node:lts-buster
+RUN apt-get update && \
+apt-get install -y \
+ffmpeg \
+imagemagick \
+webp && \
+apt-get upgrade -y && \
+rm -rf /var/lib/apt/lists/*
 
 RUN git clone https://github.com/testando0/HutaoBot-MD
 
